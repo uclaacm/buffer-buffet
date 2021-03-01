aafb836f36ce6847fd36652f3db12a9fb260b15c
Jamie Liu <jamie.liu386@gmail.com>
2021-02-28 21:09:27 -0500

    Run code through formatter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
-import './App.css';
-import illo from './illo.svg';
-import p1 from './p1 (1).svg';
-import p2 from './p2 (1).svg';
-import chef from './chef.svg';
+import React, { useState } from "react";
+import "./App.css";
+import illo from "./illo.svg";
+import p1 from "./p1 (1).svg";
+import p2 from "./p2 (1).svg";
+import chef from "./chef.svg";
 
-{/* TO DO:
+{
+  /* TO DO:
     fix styling: how to pass styles down?
     fix grid of image on title screen
     fix spacing it is very ugly oops
     do interactive parts
     implement interactivity
     add scrollspy
-*/}
+*/
+}
 //images
 
 //functions
@@ -36,88 +38,105 @@ function Heading2(props) {
 
 //frames
 function Title() {
-  return(
-    <div className = "light frame titleContainer">
+  return (
+    <div className="light frame titleContainer">
       <div>
-        <Heading head1="WELCOME TO" head2="Buffer Buffet" ></Heading>
-        <p>Did you say “table for one”? Thanks for visiting Buffer Buffet! Today, we’ll be serving you a full-course meal on the concept of buffer overflow, courtesy of your favorite code chefs from Teach LA.</p>
+        <Heading head1="WELCOME TO" head2="Buffer Buffet"></Heading>
+        <p>
+          Did you say “table for one”? Thanks for visiting Buffer Buffet! Today,
+          we’ll be serving you a full-course meal on the concept of buffer
+          overflow, courtesy of your favorite code chefs from Teach LA.
+        </p>
       </div>
-      <img id = "illo" src={illo} alt="Couldn't Find Image" />
+      <img id="illo" src={illo} alt="Couldn't Find Image" />
     </div>
   );
 }
 
 function Introduction() {
-  return(
-    <div className = "dark frame">
-        <Heading head1="INTRODUCTION" head2="What is Buffer Overflow?"></Heading>
-        
-        <p>To satisfy your hunger for knowledge, we’ll start with a quick intro
-        of what buffer overflow is.</p>     
+  return (
+    <div className="dark frame">
+      <Heading head1="INTRODUCTION" head2="What is Buffer Overflow?"></Heading>
 
-    <br/>
+      <p>
+        To satisfy your hunger for knowledge, we’ll start with a quick intro of
+        what buffer overflow is.
+      </p>
 
-    <h4 className = "lightBlue"> Ok, so what’s a buffer?</h4>
-    <p>
-      <span style = {{textDecorationLine: 'underline'}}>Buffers</span> are areas of memory set aside to hold data temporaily as it is
-      being transferred from one region to another. It can hold anything
-      from character strings to arrays of integers. As is in memory systems
-      or programs, the sizes of these buffers are typically well-defined.
-    </p>
-    <p>
-      For example, a program could contain a 8-byte buffer for a user to
-      enter their password.
-    </p>
+      <br />
 
-    <div> <img className = "password" src={p1} alt="Couldn't Find Image" /> </div>
+      <h4 className="lightBlue"> Ok, so what’s a buffer?</h4>
+      <p>
+        <span style={{ textDecorationLine: "underline" }}>Buffers</span> are
+        areas of memory set aside to hold data temporaily as it is being
+        transferred from one region to another. It can hold anything from
+        character strings to arrays of integers. As is in memory systems or
+        programs, the sizes of these buffers are typically well-defined.
+      </p>
+      <p>
+        For example, a program could contain a 8-byte buffer for a user to enter
+        their password.
+      </p>
 
-    <h4 className = "lightBlue">How do buffers overflow?</h4>
-    <p>
-    <span style = {{textDecorationLine: 'underline'}}>Buffer overflow</span> happens when a program attempts to input more data in
-      a buffer than it can hold. As a result, the program ends up going past
-      the buffer's boundary and overwrites adjacent memory locations.
-    </p>
+      <div>
+        {" "}
+        <img className="password" src={p1} alt="Couldn't Find Image" />{" "}
+      </div>
 
-    <br/>
+      <h4 className="lightBlue">How do buffers overflow?</h4>
+      <p>
+        <span style={{ textDecorationLine: "underline" }}>Buffer overflow</span>{" "}
+        happens when a program attempts to input more data in a buffer than it
+        can hold. As a result, the program ends up going past the buffer's
+        boundary and overwrites adjacent memory locations.
+      </p>
 
-    <img className = "password" src={p2} alt="Couldn't Find Image"/>
+      <br />
 
-    <br/>
+      <img className="password" src={p2} alt="Couldn't Find Image" />
 
-    <p>
-      Using the example above, the buffer would overflow if a user attempts
-      to enter in data that exceeds the 8 bytes alloted.
-    </p>
+      <br />
 
+      <p>
+        Using the example above, the buffer would overflow if a user attempts to
+        enter in data that exceeds the 8 bytes alloted.
+      </p>
     </div>
   );
 }
 
 function Transition() {
-  return(
-    <div className = "light frame">
-        <Heading head1="AN ANALOGY" head2="Buffer Overflow in the Kitchen" ></Heading>
-        <div className = "transitionContainer">
-          <img id = "chef" src={chef} alt="Couldn't Find Image" />
-          <p id="chefText" >To better illustrate the concept of buffer overflow, let’s cook up an analogy. Say you’re eating at a restaurant - what usually happens is that a waiter comes over, takes your order, writes it down on a notepad, then hands over these orders to the chef.</p>
-        </div>
-        
+  return (
+    <div className="light frame">
+      <Heading
+        head1="AN ANALOGY"
+        head2="Buffer Overflow in the Kitchen"
+      ></Heading>
+      <div className="transitionContainer">
+        <img id="chef" src={chef} alt="Couldn't Find Image" />
+        <p id="chefText">
+          To better illustrate the concept of buffer overflow, let’s cook up an
+          analogy. Say you’re eating at a restaurant - what usually happens is
+          that a waiter comes over, takes your order, writes it down on a
+          notepad, then hands over these orders to the chef.
+        </p>
       </div>
+    </div>
   );
 }
 
 function SmallTransition(props) {
-  return(
-    <div className = "light frame">
-        <h1>{props.heading}</h1>
-        <p>{props.text}</p>
-      </div>
+  return (
+    <div className="light frame">
+      <h1>{props.heading}</h1>
+      <p>{props.text}</p>
+    </div>
   );
 }
 
 function App() {
-
-  const [orders, setOrders] = useState([   // made orders into a state variable
+  const [orders, setOrders] = useState([
+    // made orders into a state variable
     ["Chicken Wings", "Apple Pie"],
     ["Pancakes", "Grilled Cheese"],
     ["Apple Pie", ""],
@@ -125,29 +144,28 @@ function App() {
     ["Chicken Wings", "Caesar Salad"],
   ]);
 
-  function OrderItem(props){
-    return(
-      <div className = "OrderItem">
-        <p style = {{fontWeight: 600}}>Order {props.num}</p>
+  function OrderItem(props) {
+    return (
+      <div className="OrderItem">
+        <p style={{ fontWeight: 600 }}>Order {props.num}</p>
         <p>{props.items[0]}</p>
         <p>{props.items[1]}</p>
       </div>
     );
   }
-  
-  function Orders(props){
-    return(
-      <div className = "Orders">
-        <h3 className = "OrderTitle">ORDERS</h3>
-        <div className = "OrderContainer">
-          <div className = "empty">
-            <p className = "question">?</p>
+
+  function Orders(props) {
+    return (
+      <div className="Orders">
+        <h3 className="OrderTitle">ORDERS</h3>
+        <div className="OrderContainer">
+          <div className="empty">
+            <p className="question">?</p>
           </div>
-          {
-            orders.map((order, i) => { // like the menuItems, we should use "map" to systematically create the React components
-              return <OrderItem key={i} num={String(i + 1)} items={order} />;
-            })
-          }
+          {orders.map((order, i) => {
+            // like the menuItems, we should use "map" to systematically create the React components
+            return <OrderItem key={i} num={String(i + 1)} items={order} />;
+          })}
         </div>
       </div>
     );
@@ -161,32 +179,41 @@ function App() {
     "Caesar Salad",
   ];
 
-  const [menuChecked, setMenuChecked] = useState([false, false, false, false, false]);
+  const [menuChecked, setMenuChecked] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
 
-  function MenuItem(props){
-    return(
+  function MenuItem(props) {
+    return (
       <div className="MenuItem">
-        <label class="container"> {props.item}
-        <input
-          type="checkbox"
-          checked={menuChecked[props.index]}                    // use appropriate value from "menuChecked" array
-          onChange={e => {
-            const newMenuChecked = [...menuChecked];            // make a copy of "menuChecked"
-            newMenuChecked[props.index] = e.target.checked;     // change the target element
-            setMenuChecked(newMenuChecked);                     // update the state with the new array
-          }}
-        />
-        <span class="checkmark"></span>
+        <label class="container">
+          {" "}
+          {props.item}
+          <input
+            type="checkbox"
+            checked={menuChecked[props.index]} // use appropriate value from "menuChecked" array
+            onChange={(e) => {
+              const newMenuChecked = [...menuChecked]; // make a copy of "menuChecked"
+              newMenuChecked[props.index] = e.target.checked; // change the target element
+              setMenuChecked(newMenuChecked); // update the state with the new array
+            }}
+          />
+          <span class="checkmark"></span>
         </label>
       </div>
     );
   }
 
   function ReadOrder() {
-    const newOrders = [...orders];         // make a copy of orders to manipulate
+    const newOrders = [...orders]; // make a copy of orders to manipulate
     let checkedItems = [];
-    for(let i = 0; i < menu.length; i++){
-      if (menuChecked[i]) {                // we should check our "menuChecked" state instead of trying to access the page elements
+    for (let i = 0; i < menu.length; i++) {
+      if (menuChecked[i]) {
+        // we should check our "menuChecked" state instead of trying to access the page elements
         checkedItems.push(menu[i]);
         if (checkedItems.length === 2) {
           newOrders.push(checkedItems);
@@ -198,7 +225,7 @@ function App() {
       checkedItems.push("");
       newOrders.push(checkedItems);
     }
-    setOrders(newOrders);                 // update the orders once we're done processing
+    setOrders(newOrders); // update the orders once we're done processing
   }
 
   // used "map" to systematically create one MenuItem for each item in the "menu" array
@@ -207,38 +234,51 @@ function App() {
     return <MenuItem key={item} item={item} index={i} />;
   });
 
-  function Menu(){
-    return(
-      <div className = "Menu center">
-        <h3 className = "MenuTitle">MENU</h3>
-          {menuItems}
-        <button onClick = {ReadOrder}>Submit</button>
+  function Menu() {
+    return (
+      <div className="Menu center">
+        <h3 className="MenuTitle">MENU</h3>
+        {menuItems}
+        <button onClick={ReadOrder}>Submit</button>
       </div>
     );
   }
 
   function Interactive(props) {
-    return(
-      <div className = "dark frame">
-          <Heading2 head1={props.head1} head2={props.head2} ></Heading2>
-          <div className = "interactive">
-            <Menu></Menu>
-            <Orders arr={props.arr}></Orders>
-          </div>
+    return (
+      <div className="dark frame">
+        <Heading2 head1={props.head1} head2={props.head2}></Heading2>
+        <div className="interactive">
+          <Menu></Menu>
+          <Orders arr={props.arr}></Orders>
+        </div>
       </div>
     );
   }
-  
+
   return (
     <div className="App">
-      <Title/>
-      <Introduction/> 
-      <Transition/>
-      <Interactive head1 = 'Make some orders' head2 = "PICK 2 THINGS OFF THE MENU" arr = {orders}></Interactive>
-      <SmallTransition heading = "Now let's cause a buffer overflow..." text = ""></SmallTransition>
-      <Interactive head1 = 'Make a long order' head2 = "PICK 3 OR MORE THINGS OFF THE MENU"></Interactive>
-      <SmallTransition heading = "That's Buffer Overflow!" text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing enim eu. Lacinia quis vel eros donec ac odio tempor orci dapibus. Now try it with code!"></SmallTransition>
-  {/*<body>
+      <Title />
+      <Introduction />
+      <Transition />
+      <Interactive
+        head1="Make some orders"
+        head2="PICK 2 THINGS OFF THE MENU"
+        arr={orders}
+      ></Interactive>
+      <SmallTransition
+        heading="Now let's cause a buffer overflow..."
+        text=""
+      ></SmallTransition>
+      <Interactive
+        head1="Make a long order"
+        head2="PICK 3 OR MORE THINGS OFF THE MENU"
+      ></Interactive>
+      <SmallTransition
+        heading="That's Buffer Overflow!"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing enim eu. Lacinia quis vel eros donec ac odio tempor orci dapibus. Now try it with code!"
+      ></SmallTransition>
+      {/*<body>
     
   </div>
     <div className = "light frame">
@@ -248,7 +288,7 @@ function App() {
          
     </div>
   </body>*/}
-  </div>
+    </div>
   );
 }
 
